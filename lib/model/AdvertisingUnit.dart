3f8b2c26d7e1066d6e488c8275b3f4112db377f5
Mohamed Hesham <mohamed.hesham-gamal@valeo.com>
2019-08-15 17:26:39 +0200

    [DEV] [Business] Adding [Ad,AdvertisingUnit,User] model

---
+++ lib/model/AdvertisingUnit.dart
@@ -0,0 +1,8 @@
+import 'package:iad_advertiser/model/Ad.dart';
+import 'package:iad_advertiser/model/AdvertisingChannel.dart';
+
+class AdvertisingUnit{
+  AdvertisingChannel advertisingChannel;
+  DateTime adDateTime;
+  Ad ad;
+}
